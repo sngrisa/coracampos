aa17316b6a716492eeea43f91c9e6aabacde4e5b
Santiago <grisansantiago@gmail.com>
2024-03-02 11:06:43 -0300

    fix select cora campos

--- coracampos-front-end/src/app/pages/biodiversity/biodiversity.component.ts
+++ coracampos-front-end/src/app/pages/biodiversity/biodiversity.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { BiodiversityService } from './services/biodiversity.service';
 import { BiodiversityCard } from './interfaces/bioDiversity.interface';
 
@@ -7,11 +7,13 @@ import { BiodiversityCard } from './interfaces/bioDiversity.interface';
   templateUrl: './biodiversity.component.html',
   styleUrl: './biodiversity.component.scss'
 })
-export class BiodiversityComponent implements OnInit {
+export class BiodiversityComponent implements OnInit, AfterViewInit {
 
   urlMercadoLibre: string = "https://inmuebles.mercadolibre.com.ar/_CustId_165236308";
 
   cardsBio!: BiodiversityCard[];
+
+  @ViewChild('filterProperties') valueSelect!: any;
 
   constructor(private bioDiversityService: BiodiversityService) { }
 
@@ -21,6 +23,36 @@ export class BiodiversityComponent implements OnInit {
     })
   }
 
+  ngAfterViewInit(): void {
+    this.bioDiversityService.getBioDiversityProperties().subscribe((properties: BiodiversityCard[]) => {
+      return this.cardsBio = properties;
+    })
+  }
 
 
+  compareByName = (a: any, b: any) => {
+    if (a.title < b.title) { return -1 }
+    return (a.title > b.title) ? 1 : 0;
+  }
+
+
+  compareByPrice = (a: any, b: any) => {
+    if (a.price < b.price) { return -1 }
+    return (a.price > b.price) ? 1 : 0;
+  }
+
+  orderProperties = () => {
+    switch (this.valueSelect.nativeElement.value) {
+      case '3':
+        this.cardsBio.sort(this.compareByName);
+        break;
+      case '1':
+        this.cardsBio.sort(this.compareByPrice);
+        break;
+      case '2':
+        this.cardsBio.sort(this.compareByPrice).reverse();
+        break;
+    }
+
+  }
 }
--- coracampos-front-end/src/app/pages/properties/grid-properties/grid-properties.component.ts
+++ coracampos-front-end/src/app/pages/properties/grid-properties/grid-properties.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Property } from '../interfaces/property.interface';
 import { PropertyService } from '../services/property.service';
 
@@ -7,14 +7,49 @@ import { PropertyService } from '../services/property.service';
   templateUrl: './grid-properties.component.html',
   styleUrl: './grid-properties.component.scss'
 })
+
 export class GridPropertiesComponent implements OnInit {
 
   constructor(private propertyService: PropertyService) { }
 
   properties!: Property[];
 
+  @ViewChild('filterProperties') valueSelect!: any;
+
   ngOnInit(): void {
     this.propertyService.getProperties().subscribe((data: Property[]) => this.properties = data);
   }
+
+  ngAfterView(): void {
+    this.propertyService.getProperties().subscribe((data: Property[]) => this.properties = data);
+  }
+
+
+  compareByName = (a: any, b: any) => {
+    if (a.title < b.title) { return -1 }
+    return (a.title > b.title) ? 1 : 0;
+  }
+
+
+  compareByPrice = (a: any, b: any) => {
+    if (a.price < b.price) { return -1 }
+    return (a.price > b.price) ? 1 : 0;
+  }
+
+  orderProperties = () => {
+    switch (this.valueSelect.nativeElement.value) {
+      case '3':
+        this.properties.sort(this.compareByName);
+        break;
+      case '1':
+        this.properties.sort(this.compareByPrice);
+        break;
+      case '2':
+        this.properties.sort(this.compareByPrice).reverse();
+        break;
+    }
+  }
+
+
 
 }
